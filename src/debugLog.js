1f7651ede33758203930239fb216c4acae025485
Christian Vuerings <christian@cambly.com>
2024-08-05 06:53:34 +0200

    Debug windows test failures

--- src/debugLog.js
+++ src/debugLog.js
@@ -1,6 +1,8 @@
 import { debuglog } from "util";
 
 export default function debugLog(...args) {
-  const log = debuglog("nightly-node");
-  log(...args);
+  // const log = debuglog("nightly-node");
+  // log(...args);
+
+  console.log(...args);
 }
